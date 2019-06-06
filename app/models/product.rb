81133819f7a78534875bd0211233ca198359e9c4
shownola <swasieko@gmail.com>
2019-06-05 22:05:16 -0700

    Add validations to Products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ApplicationRecord
+
+  has_many :product_categories
+  has_many :categories, through: :product_categories
+
+  validates :title, :price, :cost presence: true
+  validates :title, length: { maximum: 144, too_long: "%{count} character is the maximum allowed" }
+  validates :description,  length: { maximum: 1000, too_long: "%(count) characters is the maximum allowed" }
+  validates :price, numericality: true
+
+
 end
