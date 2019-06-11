7c40b1bdeaec1fec74dd10b755397701cecb461a
shownola <swasieko@gmail.com>
2019-06-11 14:30:13 -0700

    Add devise for authentication and bootstrap for styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admins, path: '', path_name: {sign_in: 'login', sign_out: 'logout', register: 'sign_up'}
+  devise_for :customers, path: '', path_name: {sign_in: 'login', sign_out: 'logout', register: 'sign_up'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'categories#index'
